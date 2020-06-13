cbf076c41a3b08e3a3f0aa883b1e69ece106fa2a
Administrator <Administrator@IK-PC>
2020-06-13 19:34:16 +0530

    First Commit

---
+++ src/test/java/com/Library/Events/LibraryEventsProducer/LibraryEventsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Library.Events.LibraryEventsProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
