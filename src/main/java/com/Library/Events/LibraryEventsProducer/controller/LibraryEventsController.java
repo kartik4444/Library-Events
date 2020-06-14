bfcbf9e526ccc0a8f1d2d30342d2d6a9882eb581
kartik patel <kartikpatell4july1997@gmail.com>
2020-06-14 10:02:49 +0530

    Added Aysnc and Sync call for sending events to kafka

--- src/main/java/com/Library/Events/LibraryEventsProducer/controller/LibraryEventsController.java
+++ src/main/java/com/Library/Events/LibraryEventsProducer/controller/LibraryEventsController.java
@@ -5,25 +5,33 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.Library.Events.LibraryEventsProducer.model.LibraryEvent;
 import com.Library.Events.LibraryEventsProducer.producer.LibraryEventProducer;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 @Controller
 public class LibraryEventsController {
-	Logger logger =LoggerFactory.getLogger(LibraryEventsController.class);
-	
+	Logger logger = LoggerFactory.getLogger(LibraryEventsController.class);
+
 	@Autowired
 	private LibraryEventProducer libraryEventProducer;
-	
-	@PostMapping(value = "/v1/publishLibraryEvent")
-	public ResponseEntity<String> produceLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException{
-		logger.info("Producing Library Events to topic Library-Events");
-		libraryEventProducer.sendLibraryEvent(libraryEvent);
+
+	@PostMapping(value = "/v1/publishLibraryEventAsync")
+	public ResponseEntity<String> produceLibraryEventAsync(@RequestBody LibraryEvent libraryEvent) throws Exception {
+		logger.info("Asynchronuosly Producing Library Events to topic Library-Events");
+		libraryEventProducer.postLibraryEvent(libraryEvent);
 		return new ResponseEntity<String>(libraryEvent.toString(), HttpStatus.CREATED);
+	}
+
+	@PostMapping(value = "/v1/publishLibraryEventSync")
+	public ResponseEntity<String> produceLibraryEventSync(@RequestBody LibraryEvent libraryEvent) throws Exception {
+		logger.info("Synchronuosly Producing Library Events to topic Library-Events");
+		SendResult<Integer, String> sendResult = libraryEventProducer.postLibraryEventSynchronous(libraryEvent);
+		logger.info("After produceLibraryEvent ,Result {}", sendResult);
+		return new ResponseEntity<String>(sendResult.toString(), HttpStatus.CREATED);
 	}
 
 }
--- src/main/java/com/Library/Events/LibraryEventsProducer/producer/LibraryEventProducer.java
+++ src/main/java/com/Library/Events/LibraryEventsProducer/producer/LibraryEventProducer.java
@@ -1,5 +1,7 @@
 package com.Library.Events.LibraryEventsProducer.producer;
 
+import java.util.concurrent.ExecutionException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,49 +17,70 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class LibraryEventProducer {
-	Logger logger =LoggerFactory.getLogger(LibraryEventProducer.class);
-	
+	Logger logger = LoggerFactory.getLogger(LibraryEventProducer.class);
+
 	@Autowired
 	private KafkaTemplate<Integer, String> kafkaTemplate;
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
-		Integer key=libraryEvent.getLibraryEventId();
-		String value = objectMapper.writeValueAsString(libraryEvent); 
-	ListenableFuture<SendResult<Integer, String>> listenableFuture= kafkaTemplate.sendDefault(key, value);
-	listenableFuture.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
+	/*
+	 * To Send Library Events ASynchronously to Kafka topic. User will get created
+	 * response as soon as an library event is scanned irrespective of sending is
+	 * success or failed
+	 */
 
-		@Override
-		public void onSuccess(SendResult<Integer, String> result) {
-			handleSuccess(key,value,result);
-		}
+	public void postLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
+		Integer key = libraryEvent.getLibraryEventId();
+		String value = objectMapper.writeValueAsString(libraryEvent);
+		ListenableFuture<SendResult<Integer, String>> listenableFuture = kafkaTemplate.sendDefault(key, value);
+		listenableFuture.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
+			@Override
+			public void onSuccess(SendResult<Integer, String> result) {
+				logger.info("Message sent Successfully for key {} and value {} , partition {}", key, value,
+						result.getRecordMetadata().partition());
+			}
 
-		@Override
-		public void onFailure(Throwable ex) {
-			handleFailure(key,value,ex);
-			
-		}
-	});
+			@Override
+			public void onFailure(Throwable ex) {
+				handleFailure(key, value, ex);
+
+			}
+		});
 	}
-	
-	
+
 	public void handleFailure(Integer key, String value, Throwable ex) {
-   logger.error("Error sending message to topic:: exception is {}",ex);
-   try {
-	   throw ex;
-   }
-   catch(Throwable e) {
-	   logger.error("Exception occurred in sending message ",e);
-   }
-		
+		logger.error("Error sending message to topic:: exception is {}", ex);
+		try {
+			throw ex;
+		} catch (Throwable e) {
+			logger.error("Exception occurred in sending message ", e);
+		}
+
 	}
 
+	/*
+	 * To Send Library Events Synchronously to Kafka topic User will get created
+	 * response only when it is successfully send to kafka topic
+	 */
 
+	public SendResult<Integer, String> postLibraryEventSynchronous(LibraryEvent libraryEvent) throws Exception {
+		Integer key = libraryEvent.getLibraryEventId();
+		String value = objectMapper.writeValueAsString(libraryEvent);
+		SendResult<Integer, String> sendResult = null;
+		try {
+			sendResult = kafkaTemplate.sendDefault(key, value).get();
+		} catch (ExecutionException | InterruptedException e) {
+			logger.error("ExecutionException/InterruptedException occurred in sending event to kafka,exception is {}",
+					e.getMessage());
+			throw e;
+		} catch (Exception e) {
+			logger.error("Exception occurred in sending event to kafka,exception is {}", e.getMessage());
+			throw e;
 
-	public void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
-		logger.info("Message sent Successfully for key {} and value {} , partition {}",key,value,result.getRecordMetadata().partition());
-			
+		}
+		return sendResult;
+
 	}
-	
+
 }
